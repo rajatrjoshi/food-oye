b08bb906d9a9b51bf43fcfe08dcab470c7e0299e
Harsh Kapadia <harshgkapadia@gmail.com>
2020-09-01 19:06:28 +0530

    :sparkles: feat: Add cart functionality and improve checkout button

--- public/js/dishes.js
+++ public/js/dishes.js
@@ -1,8 +1,10 @@
 const button = document.querySelectorAll(".get-data");
+const dishCount = document.querySelector("#dish-count");
 
-let dishes = [];
+let dishCounter = 0
+
+let dishes = JSON.parse(sessionStorage.getItem("fo_dishes"));
 let dishesLength = 0;
-dishes = JSON.parse(sessionStorage.getItem("fo_dishes"));
 if(dishes != null)
 	dishesLength = dishes.length;
 else
@@ -16,22 +18,45 @@ for(let i = 0; i < button.length; i++)
 		() =>
 		{
 			let dishId = button[i].value;
-			let isPresent = false;
-			for(let j = 0; j < dishesLength; j++)
-			{
-				if(dishId === dishes[j])
-				{
-					isPresent = true;
-					break;
-				}
-			}
+			// let isPresent = false;
+			// for(let j = 0; j < dishesLength; j++)
+			// {
+			// 	if(dishId === dishes[j])
+			// 	{
+			// 		isPresent = true;
+			// 		break;
+			// 	}
+			// }
 
-			if(isPresent === false)
+			// if(isPresent === false)
+			// {
+			// 	dishes.push(dishId);
+			// 	button[i].innerHTML="ADDED";
+			// 	dishCounter++;
+			// 	dishCount.innerText = dishCounter;
+			// 	// button[i].disabled = true;
+			// 	sessionStorage.setItem("fo_dishes", JSON.stringify(dishes));
+			// }
+
+			if(button[i].innerText === "ADD TO CART")
 			{
 				dishes.push(dishId);
-				button[i].innerHTML="ADDED";
-				button[i].disabled = true;
 				sessionStorage.setItem("fo_dishes", JSON.stringify(dishes));
+
+				dishCounter++;
+				dishCount.innerText = dishCounter;
+
+				button[i].innerText="ADDED";
+			}
+			else
+			{
+				dishes = dishes.filter(dish => dishId != dish);
+				sessionStorage.setItem("fo_dishes", JSON.stringify(dishes));
+
+				dishCounter--;
+				dishCount.innerText = dishCounter;
+
+				button[i].innerText="ADD TO CART";
 			}
 		}
 	);
@@ -44,7 +69,7 @@ window.addEventListener
 	{
 		const dishesArr = JSON.parse(sessionStorage.getItem("fo_dishes"));
 
-		if(dishesArr)
+		if(dishesArr && dishesArr.length > 0)
 		{
 			for(let i = 0; i < button.length; i++)
 			{
@@ -53,10 +78,12 @@ window.addEventListener
 					if(dishesArr[j] === button[i].value)
 					{
 						button[i].innerHTML = "ADDED";
-						button[i].disabled = true;
+						dishCounter++;
 					}
 				}
 			}
+
+			dishCount.innerText = dishCounter;
 		}
 	}	
 );
